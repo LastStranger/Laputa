2a35520f462e6506b3f0133c8865610214f468c7
lizhen <lizhen@gaodun.com>
2018-11-28 11:01:48 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path');
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },
+                ]
+            },
+
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+             template: 'app/index.html'
+        })
+    ]
+};
